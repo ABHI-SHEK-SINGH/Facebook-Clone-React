bed248110cc0241f2aad7a18c4936f5a051f61e1
ABHI-SHEK-SINGH <104828515+ABHI-SHEK-SINGH@users.noreply.github.com>
2022-09-19 17:17:12 +0530

    Update harsh.js

--- src/harsh.js
+++ src/harsh.js
@@ -6,14 +6,7 @@ import { FacebookAuthProvider } from "firebase/auth";
 import 'firebase/storage'; 
 import "firebase/compat/storage";
 
-const firebaseConfig = harsh.initializeApp({
-    apiKey: "AIzaSyCz_F8UceV7_HTvVHlVbAWA1Iz2a44nYq0",
-    authDomain: "fb-clone-ee422.firebaseapp.com",
-    projectId: "fb-clone-ee422",
-    storageBucket: "fb-clone-ee422.appspot.com",
-    messagingSenderId: "493623538499",
-    appId: "1:493623538499:web:02330e9d15617adacf35f6"
-  });
+
     
 // Initialize Firebase
 const auth = harsh.auth();
